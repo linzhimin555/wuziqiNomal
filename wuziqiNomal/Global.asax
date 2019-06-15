1f825ba20d3bba5a3c7820bb5036ce841f505925
linzhimin555 <1137020867@qq.com>
2019-06-15 17:56:24 +0800

    111

---
+++ wuziqiNomal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wuziqiNomal.MvcApplication" Language="C#" %>
---
+++ wuziqiNomal/Models/Room.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace wuziqiNomal.Models
+{
+    public class Room
+    {
+        public long RoomId { get; set; }
+
+        public string RoomName { get; set; }
+
+        public List<RoomUser> Users { get; set; }
+
+        public bool Start { get; set; } = false;
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Ip { get; set; }
+
+        public int Sex { get; set; }
+
+        public string RoomId { get; set; }
+    }
+
+    public class RoomUser
+    {
+        public string Id { get; set; }
+
+        public bool Ready { get; set; } = false;
+
+        public Chess Color { get; set; }
+    }
+    public enum Chess
+    {
+        Black = 1,
+        Red = -1,
+        Look = 0
+    }
+}

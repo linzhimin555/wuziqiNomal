daf2e827cdf0eeb956a89af0fd5aa85934514abf
1137020867@qq.com <555gudanshijie>
2019-06-16 21:43:07 +0800

    1

---
+++ wuziqiNomal/App_Start/Startup.Auth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace wuziqiNomal
+{
+    public partial class Startup
+    {
+        void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Chezugou",
+                LoginPath = new PathString(ConfigurationManager.AppSettings["LoginPath"]),
+                CookieDomain = ConfigurationManager.AppSettings["CookieDomain"]
+            });
+        }
+    }
+}
---
+++ wuziqiNomal/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet;
+
+[assembly: OwinStartup(typeof(wuziqiNomal.Startup))]
+namespace wuziqiNomal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
